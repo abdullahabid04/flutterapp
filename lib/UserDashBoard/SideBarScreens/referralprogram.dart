5bc5fddaa56dc8b9d831a3f764856bad9d5b995a
@pyuserabdullah <abdrps2004@gmail.com>
2022-05-17 15:02:13 +0500

    fifth commit

--- lib/UserDashBoard/SideBarScreens/referralprogram.dart
+++ lib/UserDashBoard/SideBarScreens/referralprogram.dart
@@ -16,6 +16,69 @@ class ReferProgram extends StatelessWidget {
           alignment: Alignment.center,
           child: Text("My Referrals"),
         ),
+        Container(
+          height: size.height * 0.03,
+          alignment: Alignment.center,
+          child: Text("Referral 1"),
+        ),
+        Container(
+          child: Column(children: [
+            RoundedInputField(
+              hintText: "Referral 1 name",
+            ),
+            Container(
+              height: size.height * 0.01,
+            ),
+            RoundedInputField(
+              hintText: "Referral 1 phone",
+            ),
+          ]),
+        ),
+        Container(
+          height: size.height * 0.03,
+          alignment: Alignment.center,
+          child: Text("Referral 2"),
+        ),
+        Container(
+          child: Column(children: [
+            RoundedInputField(
+              hintText: "Referral 2 name",
+            ),
+            Container(
+              height: size.height * 0.01,
+            ),
+            RoundedInputField(
+              hintText: "Referral 2 phone",
+            ),
+          ]),
+        ),
+        Container(
+          height: size.height * 0.03,
+          alignment: Alignment.center,
+          child: Text("Referral 3"),
+        ),
+        Container(
+          child: Column(children: [
+            RoundedInputField(
+              hintText: "Referral 3 name",
+            ),
+            Container(
+              height: size.height * 0.01,
+            ),
+            RoundedInputField(
+              hintText: "Referral 3 phone",
+            ),
+          ]),
+        ),
+        Container(
+          height: size.height * 0.05,
+        ),
+        Container(
+          child: RoundedButton(
+            text: "Refer",
+            press: () => {},
+          ),
+        ),
       ]),
     );
   }
